da6d16ace9ec56193604af8656a0b1356a36b7ee
51mpleDev <firdavs02shukurov@gmail.com>
2026-06-15 19:16:07 +0500

    add company routes to api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,20 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+
+
+});
+
+Route::prefix('company')->group(function () {
+    Route::post('/', [CompanyController::class, 'store']);
+    Route::get('/{company}', [CompanyController::class, 'show']);
+    Route::post('/{company}/refresh', [CompanyController::class, 'refresh']);
 });
