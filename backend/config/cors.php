9b5d53c56fde4a4e4b9cc5abf1e9dba535ae12c7
51mpleDev <firdavs02shukurov@gmail.com>
2026-06-18 09:05:42 +0500

    update config

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,11 +6,15 @@ return [
         'sanctum/csrf-cookie',
         'login',
         'logout',
+        'auth'
     ],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'http://localhost:15173',
+        'http://127.0.0.1:15173',
+        'http://0.0.0.0:15173',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://0.0.0.0:5173',
